cc8066826aa10e823d0f89bc6bc5dd479dbb6abd
ksshreya <138116845+ksshreya@users.noreply.github.com>
2025-04-08 21:43:01 +0530

    Minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Login from '.components/Login'
+import Login from "./components/Login";
 import { useEffect, useState } from 'react'
 import './App.css'
 import TaskInput from './components/TaskInput'
